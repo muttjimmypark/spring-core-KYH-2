8971b20aae30477e55dbe85174be73853e6cf5e1
muttjimmypark <muttpark57@gmail.com>
2022-11-06 22:13:24 +0900

    create

---
+++ src/main/java/newhello/newcore/NewcoreApplication.java
@@ -0,0 +1,13 @@
+package newhello.newcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewcoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/newhello/newcore/NewcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package newhello.newcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
