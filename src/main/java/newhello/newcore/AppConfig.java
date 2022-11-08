8e55a477fb6a56cd08e17c885922288ca3ecfe44
muttjimmypark <muttpark57@gmail.com>
2022-11-08 17:21:16 +0900

    AnnotationConfig ApplicationContext로 스프링 전환 적용

--- src/main/java/newhello/newcore/AppConfig.java
+++ src/main/java/newhello/newcore/AppConfig.java
@@ -1,20 +1,39 @@
 package newhello.newcore;
 
+import newhello.newcore.discount.DiscountPolicy;
 import newhello.newcore.discount.FixDiscountPolicy;
+import newhello.newcore.discount.RateDiscountPolicy;
+import newhello.newcore.member.MemberRepository;
 import newhello.newcore.member.MemberService;
 import newhello.newcore.member.MemberServiceImpl;
 import newhello.newcore.member.MemoryMemberRepository;
 import newhello.newcore.order.OrderService;
 import newhello.newcore.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
--- src/main/java/newhello/newcore/MemberApp.java
+++ src/main/java/newhello/newcore/MemberApp.java
@@ -4,13 +4,22 @@ import newhello.newcore.member.Grade;
 import newhello.newcore.member.Member;
 import newhello.newcore.member.MemberService;
 import newhello.newcore.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
+
 //        MemberService memberService = new MemberServiceImpl();
-        MemberService memberService = appConfig.memberService();
+
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class); //등록된 빈이름, 반환타입
+
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
