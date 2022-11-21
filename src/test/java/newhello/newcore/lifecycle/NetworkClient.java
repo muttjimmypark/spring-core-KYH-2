1c7c5455cf19a9f310f0a7d6187846cf39399ff9
muttjimmypark <muttpark57@gmail.com>
2022-11-21 18:16:26 +0900

    bean 생명주기 콜백 구현방법 1. 인터페이스 접목

--- src/test/java/newhello/newcore/lifecycle/NetworkClient.java
+++ src/test/java/newhello/newcore/lifecycle/NetworkClient.java
@@ -1,12 +1,15 @@
 package newhello.newcore.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
+//        connect();
+//        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -20,11 +23,27 @@ public class NetworkClient {
 
     public void call(String message) {
         System.out.print("call: " + url);
-        System.out.println("message = " + message);
+        System.out.println(" message = " + message);
     }
 
     //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+
+    // 생명주기 콜백을 위한 인터페이스 구체화 부분
+    // 스프링 초창기에 나온 방법이며, 근래는 이 대신 더 나은 방법을 사용한다
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
